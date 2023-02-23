1be635d9c424d530f1892952ed10bc4b59286b19
Junior <guitar.sound12@gmail.com>
2023-02-23 11:38:33 -0500

    correciones menores y optimización

--- src/components/3Dassets/3Dmodel.js
+++ src/components/3Dassets/3Dmodel.js
@@ -1,19 +1,49 @@
 import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
+import { useState, useEffect, useRef } from "react";
 /* import { OrbitControls } from "@react-three/drei"; */
 import Bot from "./bot";
 import Camera from "./camera";
 
 const Model = () => {
+	const canvasRef = useRef(null);
+	const [visible, setVisible] = useState(false)
+
+	const observer = new IntersectionObserver((entries) => {
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+				// El elemento es visible
+				setVisible(true)
+			} else {
+				// El elemento no es visible
+				setVisible(false)
+			}
+		});
+	});
+
+	useEffect(() => {
+		if (canvasRef.current) {
+			observer.observe(canvasRef.current);
+		}
+
+		return () => {
+			if (canvasRef.current) {
+				observer.unobserve(canvasRef.current);
+			}
+		};
+	}, [canvasRef]);
+
 	return (
-		<div className="introduction__canvas">
-			<Canvas /* style={{ height: "80vh", width: "40%" }} */>
-				{/* <OrbitControls /> */}
-				<Camera />
-				<Suspense fallback={null}>
-					<Bot />
-				</Suspense>
-			</Canvas>
+		<div className="introduction__canvas" ref={canvasRef}>
+			<div className={`introduction__canvas_container ${visible ? '' : 'hidden'}`} >
+				<Canvas /* style={{ height: "80vh", width: "40%" }} */>
+					{/* <OrbitControls /> */}
+					<Camera />
+					<Suspense fallback={null}>
+						<Bot />
+					</Suspense>
+				</Canvas>
+			</div>
 		</div>
 	);
 };
